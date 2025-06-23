0506948b36cd8f2cc405ef92c969ea43a461be93
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2025-06-23 17:48:46 +0100

    remove folder creation

--- src/module.ts
+++ src/module.ts
@@ -1,12 +1,9 @@
-import fsp from "fs/promises";
 import {
   defineNuxtModule,
   addComponent,
   createResolver,
-  useLogger,
   addTemplate,
 } from "@nuxt/kit";
-import { join } from "path";
 
 export interface ModuleOptions {
   componentName?: string;
@@ -35,40 +32,9 @@ export default defineNuxtModule<ModuleOptions>({
       mode: "client",
     });
 
-    const logger = useLogger("nuxt-lottie");
-
     let lottieFolder = `${options.lottieFolder}`;
     if (lottieFolder.endsWith("/")) {
       lottieFolder = lottieFolder.slice(0, -1);
-    }
-
-    const lottiePath = `${nuxt.options.srcDir}${options.lottieFolder}`;
-
-    try {
-      const stat = await fsp.stat(lottiePath);
-      if (stat.isDirectory()) {
-        logger.info(`You have Lottie folder which is nice`);
-      }
-    } catch (err) {
-      try {
-        await fsp.mkdir(lottiePath, { recursive: true });
-        logger.info(`Lottie folder created: ${lottiePath}`);
-      } catch (mkdirError) {
-        logger.error(`Failed to create folder at ${lottiePath}: ${mkdirError}`);
-        return;
-      }
-
-      try {
-        const readmePath = join(lottiePath, "README.md");
-        await fsp.writeFile(
-          readmePath,
-          "This directory is generated for Lottie files"
-        );
-      } catch (writeError) {
-        logger.error(
-          `Failed to write README.md at ${lottiePath}: ${writeError}`
-        );
-      }
     }
 
     addTemplate({
